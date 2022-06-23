05f47052e6f06e82a7673ead956e40a3ea00acf5
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-06-23 14:20:09 +0200

    post data contact vers backend

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {faPencil} from '@fortawesome/free-solid-svg-icons'
+// import { text } from '@fortawesome/fontawesome-svg-core';
+// import {faPencil} from '@fortawesome/free-solid-svg-icons'
+import { Observable } from 'rxjs';
 import { contactUser } from 'src/app/models/contactUser';
 
 @Component({
@@ -10,18 +12,24 @@ import { contactUser } from 'src/app/models/contactUser';
 })
 export class ContactComponent implements OnInit {
 
-  faPencil=faPencil;
+  //faPencil=faPencil;
 
   user: contactUser = new contactUser();
 
-  private readonly url:string="http://localhost:8080/..."
+  private readonly url:string="http://localhost:8080"
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  // onSubmit(): Observable<contactUser>{
+  //   console.log(this.user);
+  //   return this.http.post<contactUser>("http://localhost:8080/", this.user);
+  // }
   onSubmit(){
     console.log(this.user);
-    this.http.post(this.url, this.user);
+    const json = JSON.stringify(this.user)
+    console.log(json);
+    return this.http.post(this.url, this.user).subscribe();
   }
 }
