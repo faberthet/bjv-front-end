841e7b93bbf5768746bc79c2dea1398e85520738
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-08-05 22:43:36 +0200

    sanitize htmlContent

--- src/app/admin/articles-admin/article-view/article-view.component.ts
+++ src/app/admin/articles-admin/article-view/article-view.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/models/article';
 import { ActivatedRoute} from '@angular/router';
 import { ArticlesService } from 'src/app/services/articles.service';
-
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article-view',
@@ -12,17 +12,24 @@ import { ArticlesService } from 'src/app/services/articles.service';
 export class ArticleViewComponent implements OnInit {
   id: number
   article: Article = new Article();
+  trustedContent: SafeHtml;
 
-  constructor(private route: ActivatedRoute,private articleService: ArticlesService) { }
+  constructor(private route: ActivatedRoute,private articleService: ArticlesService, private sanitized: DomSanitizer) { }
 
   ngOnInit(): void {
     
     this.id=this.route.snapshot.params['id']
 
+    //Promise.resolve( // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/await
     this.articleService.getArticleById(this.id).subscribe({
       error: error => console.log(error),
       next: res => this.article=res,
-    })
+    })//).then()
+    setTimeout (() => {//trouver une solution pour effectuer cette action après this.article=res
+     // var rp=this.article.content.replace("<img src", "<img [src]");
+      this.trustedContent=this.sanitized.bypassSecurityTrustHtml(this.article.content);
+     }, 5000);
+    //this.trustedContent=this.sanitized.bypassSecurityTrustHtml(this.article.content);
 
   }
 
