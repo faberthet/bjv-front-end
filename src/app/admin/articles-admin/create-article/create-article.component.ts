7ec6c799717667910f8b428dffa1eb8347c277e9
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2023-03-01 23:05:33 +0100

    create article

--- src/app/admin/articles-admin/create-article/create-article.component.ts
+++ src/app/admin/articles-admin/create-article/create-article.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/models/article';
 import { ArticlesService } from 'src/app/services/articles.service';
 import { Router } from '@angular/router';
-import * as CustomEditor from 'ckeditor5-custom-build/build/ckeditor'
+// import * as CustomEditor from 'ckeditor5-custom-build/build/ckeditor'
 
 
 @Component({
@@ -13,14 +13,20 @@ import * as CustomEditor from 'ckeditor5-custom-build/build/ckeditor'
 export class CreateArticleComponent implements OnInit {
 
   article: Article = new Article();
+
+  sections:{name:string}[];
+  subsections:{name:string, sectionName:string, id:number}[];
+  subsDisplay:{name?:string, sectionName:string, id:number}[];
   
   constructor(private articleService: ArticlesService,private router: Router) { }
 
   ngOnInit(): void {
     this.article.content="" //error backend lors de updatearticle si null... 
     this.article.actif=false
-    //this.article.section=""
-    //this.article.subsection=""
+    // this.article.section=""
+    // this.article.subsection=""
+
+    this.getSections();
   }
 
   saveArticle(){
@@ -38,57 +44,78 @@ export class CreateArticleComponent implements OnInit {
     x.form.controls.theme.touched=true
   }
 
-  public Editor = CustomEditor;
-  public config={
-  	toolbar: {
-      items: [
-        'heading',
-        '|',
-        'bold',
-        'italic',
-        'link',
-        'bulletedList',
-        'numberedList',
-        '|',
-        'outdent',
-        'indent',
-        '|',
-        'imageUpload',
-        'blockQuote',
-        'insertTable',
-        'mediaEmbed',
-        'undo',
-        'redo',
-        'fontBackgroundColor',
-        'fontColor',
-        'fontFamily',
-        'fontSize',
-        'highlight',
-        'imageInsert',
-        'specialCharacters',
-        'todoList',
-        'underline'
+  onSelect($event: Event){
+    this.subsections=[];// pour laisser le champ select des sous-sections vide au départ
+    this.article.subsection="";
+    console.log((<HTMLTextAreaElement>$event.target).value)
+    const value:string=(<HTMLTextAreaElement>$event.target).value
+    this.articleService.getSubsections(value).subscribe({
+      error: error => console.log(error),
+      next: res => [
+        this.subsections=<{name:string, sectionName:string,id:number}[]>res,
+        console.log(this.subsections)
       ]
-    },
-    language: 'fr',
-    image: {
-      toolbar: [
-        'imageTextAlternative',
-        'imageStyle:inline',
-        'imageStyle:block',
-        'imageStyle:side',
-        'linkImage'
-      ]
-    },
-    table: {
-      contentToolbar: [
-        'tableColumn',
-        'tableRow',
-        'mergeTableCells',
-        'tableCellProperties',
-        'tableProperties'
-      ]
-    }
+    })
   }
+
+  getSections(){
+    this.articleService.getSections().subscribe({
+      error: error => console.log(error),
+      next: res => [this.sections=<{name:string}[]>res,console.log(this.sections)]
+    })
+  }
+
+//   public Editor = CustomEditor;
+//   public config={
+//   	toolbar: {
+//       items: [
+//         'heading',
+//         '|',
+//         'bold',
+//         'italic',
+//         'link',
+//         'bulletedList',
+//         'numberedList',
+//         '|',
+//         'outdent',
+//         'indent',
+//         '|',
+//         'imageUpload',
+//         'blockQuote',
+//         'insertTable',
+//         'mediaEmbed',
+//         'undo',
+//         'redo',
+//         'fontBackgroundColor',
+//         'fontColor',
+//         'fontFamily',
+//         'fontSize',
+//         'highlight',
+//         'imageInsert',
+//         'specialCharacters',
+//         'todoList',
+//         'underline'
+//       ]
+//     },
+//     language: 'fr',
+//     image: {
+//       toolbar: [
+//         'imageTextAlternative',
+//         'imageStyle:inline',
+//         'imageStyle:block',
+//         'imageStyle:side',
+//         'linkImage'
+//       ]
+//     },
+//     table: {
+//       contentToolbar: [
+//         'tableColumn',
+//         'tableRow',
+//         'mergeTableCells',
+//         'tableCellProperties',
+//         'tableProperties'
+//       ]
+//     }
+//   }
 
 }
--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -9,37 +9,56 @@ import { ArticleWithoutContent } from '../models/article-without-content';
 })
 export class ArticlesService {
 
+  private baseUrl: string="http://localhost:5000/"
+  private articlesBaseUrl: string="http://localhost:5000/api/v1/articles"
+
+  //private baseUrl: string="https://api.breizh-jardinvivant.com/"
   //private baseUrl: string="http://localhost:5000/api/v1/articles"
   //private baseUrl: string="http://breizhjar-env.eba-tipx9yis.eu-west-3.elasticbeanstalk.com/api/v1/articles"
-  private baseUrl: string="https://api.breizh-jardinvivant.com/api/v1/articles"
+  //private articlesBaseUrl: string="https://api.breizh-jardinvivant.com/api/v1/articles"
+  
 
   constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]>{
-    return this.http.get<Article[]>(this.baseUrl)
+    return this.http.get<Article[]>(this.articlesBaseUrl)
   }
 
   getArticlesWithoutContent(): Observable<ArticleWithoutContent[]>{
-    return this.http.get<ArticleWithoutContent[]>(this.baseUrl + "/dto")
+    return this.http.get<ArticleWithoutContent[]>(this.articlesBaseUrl + "/dto")
   }
 
   addArticle(article: Article): Observable<Object>{
-    return this.http.post(this.baseUrl,article);
+    return this.http.post(this.articlesBaseUrl,article);
   }
   getArticleById(id: number): Observable<Article>{
-    return this.http.get<Article>(this.baseUrl + `/${id}`)
+    return this.http.get<Article>(this.articlesBaseUrl + `/${id}`)
   }
   updateArticle(id:number, article: Article): Observable<Object>{
-    return this.http.put(this.baseUrl + `/${id}`, article)
+    return this.http.put(this.articlesBaseUrl + `/${id}`, article)
   }
   deleteArticle(id:number): Observable<Object>{
-    return this.http.delete(this.baseUrl + `/${id}`)
+    return this.http.delete(this.articlesBaseUrl + `/${id}`)
   }
   activateArticle(id: number): Observable<Object>{
-    return this.http.put(this.baseUrl + "/activate" + `/${id}`,"")
+    return this.http.put(this.articlesBaseUrl + "/activate" + `/${id}`,"")
   }
   deactivateArticle(id: number): Observable<Object>{
-    return this.http.put(this.baseUrl + "/deactivate" + `/${id}`,"")
+    return this.http.put(this.articlesBaseUrl + "/deactivate" + `/${id}`,"")
+  }
+
+
+  getSections(): Observable<Object>{
+    return this.http.get(this.baseUrl + "sections")
+  }
+  getSubsections(section:string): Observable<Object>{
+    return this.http.get(this.baseUrl + "subsections?section="+ section)
+  }
+  addSection(section:string): Observable<Object>{
+    return this.http.post(this.baseUrl + "section",section)
+  }
+  addSubsection(subsection:{name:string, sectionName:string}): Observable<Object>{
+    return this.http.post(this.baseUrl + "section",subsection)
   }
 
 }
